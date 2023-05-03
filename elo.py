33087d6b4d2b6ad22655b4cf993c2530a48b0a8e
Victoria Reguyal <victoria.reguyal@gmail.com>
2023-05-02 21:04:42 -0400

    Sorting and random pairings

--- elo.py
+++ elo.py
@@ -1,10 +1,13 @@
+import random
+master_list = []
+
 #make the list
 def make_list():
 	ranking_list = []
 	make_list = True
 	i = 0
 	while make_list:
-		name = input("Enter name of item to rank, or type 'done' to finish")
+		name = input("Enter name of item to rank, or type 'done' to finish ")
 		if name.lower() == "done":
 			break
 		elo = 1500
@@ -21,21 +24,31 @@ def expected_result(self_elo, foe_elo):
 
 #find new rating
 def elo_update(name_1, elo_1, name_2, elo_2):
-	K=15
+	K=30
 	expected = expected_result(elo_1, elo_2)
-	result = int(input("Which do you prefer? Type '1' for " + name_1 + " and type '0' for " + name_2))
+	result = int(input("Which do you prefer? Type '1' for " + name_1 + " and type '0' for " + name_2 + " "))
 	return elo_1 + K*(result - expected), elo_2 + K*((1 - result) - (1 - expected))
 
 #match up 2 dictionary entries
 def matchup(list_name):
-	for i in range(len(list_name)):
-		for j in range(i+1, len(list_name)):
-			list_name[i]["elo"], list_name[j]["elo"] = elo_update(list_name[i]["name"],list_name[i]["elo"],list_name[j]["name"],list_name[j]["elo"])
+    for i, dict in enumerate(list_name):
+        opponents = random.sample(list_name[:i] + list_name[i+1:], 2)
+        for opp in opponents:
+            dict["elo"], opp["elo"] = elo_update(dict["name"], dict["elo"], opp["name"], opp["elo"])
+
+#key for sorting the list
+def elo_key(item):
+	return item['elo']
+
+#sort the list
+def sort_list(list_name):
+	list_name.sort(reverse=True,key=elo_key)
 
 #do the thing
 cool_list = make_list()
 for item in cool_list:
  	print(item)
 matchup(cool_list)
+sort_list(cool_list)
 for item in cool_list:
 	print(item)
